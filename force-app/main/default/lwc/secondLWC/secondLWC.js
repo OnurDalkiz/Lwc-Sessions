8d081e9c4d74718371340675948763a2523ad857
OnurDalkiz <105566923+OnurDalkiz@users.noreply.github.com>
2022-06-24 00:19:46 +0300

    v6

--- force-app/main/default/lwc/secondLWC/secondLWC.js
+++ force-app/main/default/lwc/secondLWC/secondLWC.js
@@ -1,7 +1,31 @@
 import { LightningElement } from 'lwc';
 
 export default class SecondLWC extends LightningElement {
+    //Degisken Olusturalim...
+    isim = 'Isim Giriniz';
+    
 
-    dynamicBinding = 'Test';
+    handleInput(degeri){
+        //degeri.target.value(inputun icinde ki degeri verir)
+        //degeri.target.value JS in bir ozelligi.
+        this.isim=degeri.target.value;
+    }
 
+    //Ornek Iki
+
+    name = 'John';
+
+    lastname = 'Watson';
+
+    handleChange(parametre){
+
+        this.name=parametre.target.value;
+
+    }
+
+    handleChange2(parametre){
+
+        this.lastname=parametre.target.value;
+
+    }
 }
